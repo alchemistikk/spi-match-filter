f1b499686da76076601edb5b2230b3a36e18fe88
Mike Mitchell <alchemistikk@gmail.com>
2022-02-21 16:45:03 -0500

    Add hardcoded generic table outline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import data from './add-match-data'
+import data from './match-data'
 function App() {
   return (
     <div className="App">
-      <p>{JSON.stringify(data[0])}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>League</th>
+            <th>Team 1</th>
+            <th>Team 2</th>
+            <th>Quality</th>
+            <th>Importance</th>
+            <th>Match Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{data[0]["date"]}</td>
+            <td>{data[0]["league"]}</td>
+            <td>{data[0]["team1"]}</td>
+            <td>{data[0]["team2"]}</td>
+            <td>{data[0]["quality"]}</td>
+            <td>{data[0]["importance"]}</td>
+            <td>{data[0]["match_rating"]}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
