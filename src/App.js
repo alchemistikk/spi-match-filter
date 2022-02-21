98ea67f489dd6da9a7c3954cd0e556ee9bb5bd11
Mike Mitchell <alchemistikk@gmail.com>
2022-02-21 17:11:46 -0500

    Add initial TableRow function component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import data from './match-data'
+
+function TableRow(props) {
+  return (
+    <tr>
+    <td>{props.date}</td>
+    <td>{props.league}</td>
+    <td>{props.team1}</td>
+    <td>{props.team2}</td>
+    <td>{props.quality}</td>
+    <td>{props.importance}</td>
+    <td>{props.match_rating}</td>
+    </tr>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -17,15 +32,15 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>{data[0]["date"]}</td>
-            <td>{data[0]["league"]}</td>
-            <td>{data[0]["team1"]}</td>
-            <td>{data[0]["team2"]}</td>
-            <td>{data[0]["quality"]}</td>
-            <td>{data[0]["importance"]}</td>
-            <td>{data[0]["match_rating"]}</td>
-          </tr>
+          <TableRow 
+            date={data[0]["date"]} 
+            league={data[0]["league"]}
+            team1={data[0]["team1"]}
+            team2={data[0]["team2"]}
+            quality={data[0]["quality"]}
+            importance={data[0]["importance"]}
+            match_rating={data[0]["match_rating"]}
+          />
         </tbody>
       </table>
     </div>
