a5c0199f0c9d68972874636cf5ac67ebcccc5f56
Mike Mitchell <alchemistikk@gmail.com>
2022-05-18 15:24:21 -0400

    Change best match threshold to >75

--- src/best-matches.js
+++ src/best-matches.js
@@ -1,6 +1,6 @@
 import onlyFutureMatches from './get-future-matches';
 
-let bestMatches = onlyFutureMatches.filter(match => match["match_rating"] > 85);
+let bestMatches = onlyFutureMatches.filter(match => match["match_rating"] > 75);
 
 bestMatches.sort((a, b) => a["date"] - b["date"]);
 
